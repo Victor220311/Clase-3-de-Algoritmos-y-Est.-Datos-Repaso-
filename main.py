4103887d3de0355fa3233f92e52f62adc3900852
Victor220311 <victorfrancodb@gmail.com>
2025-09-15 13:50:41 +0000

    Notas 1

---
+++ main.py
@@ -0,0 +1,18 @@
+#Repaso python
+numeros = [1,2,3,4,5,6,7,8,9,10]
+for a in numeros:
+    print(a) #Recorre elementos de una lista
+
+# [1,2,3] lista
+# (1,2,3) tupla (inmutable)
+# {1,2,3} diccionario 
+
+#Pilas: coleccion de elementos que permiten solo dos acciones: Añadir/sacar elementos
+#Ultimo elemento en salir es el primero en entrar (LIFO)
+
+#Colas: coleccion de elementos que permiten solo dos acciones: Añadir/sacar elementos
+#Primer elemento dentro es el primero fuera (FIFO)
+
+#Para guardar elementos por script se con la ruta "cd" y se coloca script
+#Todo esto usando la libreris "sys"
+
